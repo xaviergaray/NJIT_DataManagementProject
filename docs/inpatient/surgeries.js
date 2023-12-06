3fc311b1d8764813c4228ddedc540372a95d1bbb
Xavier Garay <xaviergaray0010@gmail.com>
2023-12-05 22:02:05 -0500

    Corrected reset filter issue in Rooms and added filter options for Surgeries

--- docs/inpatient/surgeries.js
+++ docs/inpatient/surgeries.js
@@ -1,7 +1,6 @@
 let SurgeryTables;
 let SurgeryTypes;
 let Patients;
-let Surgeons;
 let Employees;
 
 getName = function(ID, Table) {
@@ -51,13 +50,44 @@ populateTable = function() {
         var DateCell = document.createElement('td');
         DateCell.textContent = SurgeryTables[i].SurgeryDate;
 
-
         row.appendChild(SurgeonCell);
         row.appendChild(PatientCell);
         row.appendChild(SurgeryTypeCell);
         row.appendChild(ORCell);
         row.appendChild(DateCell);
         databaseItems.querySelector('tbody').appendChild(row);
+    }
+}
+
+function populateDropdown(dropdownId, data) {
+    // Get the dropdown element
+    var dropdown = document.getElementById(dropdownId);
+
+    // Clear the dropdown
+    dropdown.innerHTML = '';
+
+    var defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.textContent = '';
+    dropdown.appendChild(defaultOption);
+
+    // Create an option element for each item in the data array
+    if (dropdownId === 'operating-theater-list') {
+
+        data.forEach(item => {
+            var option = document.createElement('option');
+            option.value = item.OperationTheatreNumber;
+            option.textContent = item.OperationTheatreNumber;
+            dropdown.appendChild(option);
+        });
+    } else
+    {
+        data.forEach(item => {
+            var option = document.createElement('option');
+            option.value = item.ID;
+            option.textContent = item.Name;
+            dropdown.appendChild(option);
+        });
     }
 }
 
@@ -68,14 +98,55 @@ window.onload = async function() {
     response = await fetch('/get-surgery-types');
     SurgeryTypes = await response.json();
 
-    response = await fetch('/get-surgeon');
-    Surgeons = await response.json();
-
     response = await fetch('/get-patients');
     Patients = await response.json();
 
     response = await fetch('/get-employees');
     Employees = await response.json();
 
+    await populateDropdown('patient', Patients)
+    await populateDropdown('surgeon', Employees)
+    await populateDropdown('operating-theater-list', SurgeryTables)
+
     populateTable()
+
+    var surgeonFilter = document.getElementById('surgeon');
+    var patientFilter = document.getElementById('patient');
+    var ORFilter = document.getElementById('operating-theater-list');
+    var dateStartLabel = document.querySelector('label[for="dateStart"]');
+    var dateStart = document.getElementById('dateStart');
+    var rangeCheckbox = document.getElementById('rangeCheckbox');
+    var dateEnd = document.getElementById('dateEnd');
+    var resetFiltersButton = document.getElementById('reset');
+
+    surgeonFilter.addEventListener('change', function() {
+        populateTable();
+    });
+
+    patientFilter.addEventListener('change', function() {
+        populateTable();
+    });
+
+    ORFilter.addEventListener('change', function() {
+        populateTable();
+    });
+
+    rangeCheckbox.addEventListener('change', function() {
+        dateEnd.disabled = !this.checked;
+        dateStart.disabled = !this.checked;
+        populateTable();
+    });
+
+    // Reset filters
+    resetFiltersButton.addEventListener('click', function() {
+        surgeonFilter.value = '0';
+        patientFilter.value = '0';
+        ORFilter.value = '0';
+        rangeCheckbox.checked = false;
+        dateStart.value = null;
+        dateStart.disabled = true;
+        dateEnd.value = null;
+        dateEnd.disabled = true;
+        populateTable();
+    });
 }
